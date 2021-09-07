874240f537b5e649889e407c68de16650861f98c
Fu Kai <ca2pa23187@gmail.com>
2021-09-08 00:22:50 +0800

    20210908

--- say.go
+++ say.go
@@ -2,12 +2,12 @@ package go-module
 
 import "fmt" 
 
-// v1
+// v1.0.0
 func SayHiV1(name string) string {
    return fmt.Sprintf("Hi, v1.0.0 %s", name)
 }
 
-// v1
-func SayHiV2(name string) string {
-   return fmt.Sprintf("Hi, v2.0.0 %s", name)
-}
+// v1.0.1
+// func SayHiV2(name string) string {
+//    return fmt.Sprintf("Hi, v1.0.1 %s", name)
+// }
